9ab5c35241a32cb20e27da559571fafe6d3c33e7
evlinges <evelina111tkachuk@gmail.com>
2025-05-13 17:31:53 +0300

    Верстка блоку контакти та футера + адаптив. Майже готовий

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
-const burger = document.getElementById('burger');
-const menu = document.getElementById('menu');
-burger.addEventListener('click', function() {
-  burger.classList.toggle('active');
-  menu.classList.toggle('active');
+ const burger = document.getElementById('burger');
+ const menu = document.getElementById('menu');
+ burger.addEventListener('click', function() {
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+ });
+ 
+document.getElementById('contactForm').addEventListener('submit', function(e){
+  e.preventDefault();
+  this.classList.add('d-none');
+  document.querySelector('.contact-thankyou').classList.remove('d-none');
 });
