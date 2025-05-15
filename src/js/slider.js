137d9b380ef8e95406a3a639d2aabd4cf7e96cb6
evlinges <evelina111tkachuk@gmail.com>
2025-05-15 15:07:24 +0300

    Збереження змін всього проєкту

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+      dots: true,
+      arrows: false,
+      appendDots: $('.slick-dots-custom'),
+      autoplay: true,
+      autoplaySpeed: 6000,
+      adaptiveHeight: true
+    });
+  });
+
